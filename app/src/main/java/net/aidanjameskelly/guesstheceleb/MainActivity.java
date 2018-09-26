671477955e7655cb02ddfde30f70d0d64c4f6e87
Aidan Kelly <it.is.aidan@gmail.com>
2018-09-26 14:18:37 -0600

    Allows for downloading of images

--- app/src/main/java/net/aidanjameskelly/guesstheceleb/MainActivity.java
+++ app/src/main/java/net/aidanjameskelly/guesstheceleb/MainActivity.java
@@ -1,24 +1,50 @@
 package net.aidanjameskelly.guesstheceleb;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    //initialize our public variables
     Random rand;
     int celebNumber;
     String websiteHTML;
+    ArrayList<Celeb> celebList;
 
+    ImageView imageView;
+    DownloadImage downloadImage;
+
+    //our celeb class.
+    //url: a String that represents an image of the celeb
+    //celebName: a String containing the characters of the celeb's name
+    public class Celeb{
+        String url;
+        String celebName;
+
+        public  Celeb(String inUrl, String inCeleb){
+            url = inUrl;
+            celebName = inCeleb;
+        }
+
+    }
+
+
+    //Downloads the HTML of the website.
     public class DownloadHTML extends AsyncTask<String,Void,String> {
 
         @Override
@@ -39,9 +65,6 @@ public class MainActivity extends AppCompatActivity {
 
                 return result;
 
-
-
-
             } catch (Exception e) {
                 e.printStackTrace();
                 return "Failed";
@@ -50,12 +73,42 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public class DownloadImage extends  AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+
+            try{
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream in = connection.getInputStream();
+
+                Bitmap image = BitmapFactory.decodeStream(in);
+
+                return image;
+
+
+            }catch(Exception e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+    }
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = findViewById(R.id.imageView);
+
         DownloadHTML downloadHTML = new DownloadHTML();
+        downloadImage = new DownloadImage();
         websiteHTML = "";
 
         try{
@@ -65,19 +118,38 @@ public class MainActivity extends AppCompatActivity {
         }
 
         rand = new Random();
+        celebList = new ArrayList<Celeb>();
 
-
+        //we use a regex to find all the pictures of celebs and their names
         Pattern p = Pattern.compile("<img src=\"(.*?)\" alt=\"(.*?)\"/>");
         Matcher m = p.matcher(websiteHTML);
 
+        //when we find a match we create a new Celeb object and add it to our list
         while(m.find()){
-            System.out.println(m.group(1));
+            Celeb toAddCeleb = new Celeb(m.group(1), m.group(2));
+            celebList.add(toAddCeleb);
         }
+
+        newCeleb();
+
 
     }
 
+    //randomly chooses a new celeb and updates the image.
     public void newCeleb(){
-        celebNumber = rand.nextInt(87) + 1;
+        celebNumber = rand.nextInt(81);
+        Celeb celeb = celebList.get(celebNumber);
+        Bitmap image;
+        try{
+            image = downloadImage.execute(celeb.url).get();
+        }catch (Exception e){
+            e.printStackTrace();
+            image = null;
+        }
+
+        imageView.setImageBitmap(image);
+
+
     }
 
 }
